20ce268720c8b846235691ad10fbcce82bffe6eb
Кирилл Афанасьев <afanasyevkirill97@mail.ru>
2023-01-31 00:09:25 +0400

    lesson 3

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from timeit import default_timer
+from django.contrib.auth.models import Group
+from .models import Product, Order
 
 
 def shop_index(request: HttpRequest):
@@ -14,3 +16,26 @@ def shop_index(request: HttpRequest):
         'products': products
     }
     return render(request, 'shopapp/shop-index.html', context=context)
+
+
+def groups_list(request: HttpRequest):
+    context = {
+        'groups': Group.objects.prefetch_related('permissions').all(),
+    }
+    return render(request, 'shopapp/groups-list.html', context=context)
+
+
+def products_list(request: HttpRequest):
+    context = {
+        'products': Product.objects.all(),
+
+    }
+    return render(request, 'shopapp/products-list.html', context=context)
+
+
+def orders_list(request:HttpRequest):
+    context = {
+        'orders': Order.objects.select_related('user').prefetch_related('products').all(),
+
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)
