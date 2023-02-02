20be1cdd55e75cfeaaeb462d66cd2a8dbf3c5fbb
Kirill Afanasyev <kirillmas222@gmail.com>
2023-02-02 05:25:35 +0400

    lesson 4

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -3,12 +3,24 @@ from django.contrib.auth.models import User
 
 
 class Product(models.Model):
+    class Meta:
+        ordering = ['name', 'price']
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     discount = models.SmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
+
+
+    # @property
+    # def description_short(self) -> str:
+    #     if len(self.description) < 48:
+    #         return self.description
+    #     return self.description[:48] + '...'
+
+    def __str__(self) -> str:
+        return f'Product(pk={self.pk}, name = {self.name!r})'
 
 
 class Order(models.Model):
